992d28f8db76b261b933b2bb7b52dd6e56ffc9a3
Favour-Cell <74482385+Favour-Cell@users.noreply.github.com>
2021-08-29 17:08:45 +0200

    Update star.js

--- star.js
+++ star.js
@@ -1,6 +1,29 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("body-toggle");
-  }
+var element = document.body;
+var icon = document.getElementById("moon")
+var lorems = document.getElementById("lorems");
+var lorem = document.getElementById("lorem");
+var link = document.getElementById("a");
+var linke = document.getElementById("b");
+var linkee = document.getElementById("c");
+var linkeee = document.getElementById("d");
+var fbk = document.getElementById("fbk");
+var ig = document.getElementById("ig");
+var twt = document.getElementById("twt");
 
+
+function myFunction() {
+    element.classList.toggle("body-toggle");
+    lorem.classList.toggle("lorem-toggle");
+    lorems.classList.toggle("lorem-toggle");
+    link.classList.toggle("lorem-toggle");
+    linke.classList.toggle("lorem-toggle");
+    linkee.classList.toggle("lorem-toggle");
+    linkeee.classList.toggle("lorem-toggle");
+    icon.classList.toggle("lorem-toggle");
+    ig.classList.toggle("lorem-toggle");
+    fbk.classList.toggle("lorem-toggle");
+    twt.classList.toggle("lorem-toggle");
+    
+
+ }
 
